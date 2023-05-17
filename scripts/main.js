12f38ae5c5fcc7a68696267231df2734c33ee842
Juliana Madeira <juliwood1980@gmail.com>
2023-05-17 15:05:26 -0300

    feat: testing shop products images

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,196 @@
 //criar produto na pagina shop
+const products = [
+    {
+        id: 0,
+        name: "Cartoon Flower Shirt",
+        price: 150,
+        inCart: 0,
+        description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "../images/products/f1.jpg",
+        brand: "adidas"
+    },
+    {
+        id: 1,
+        name: "Coat Shirt Double Colors",
+        price: 190,
+        inCart: 0,
+        description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "../images/products/f6.jpg",
+        brand: "adidas"
+    },
+    {
+        id: 2,
+        name: "Spring Flower Shirt",
+        price: 150,
+        inCart: 0,
+        description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "../images/products/f5.jpg",
+        brand: "adidas"
+    },
+    {
+        id: 3,
+        name: "Floral Summer Pants",
+        price: 190,
+        inCart: 0,
+        description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "../images/products/f7.jpg",
+        brand: "adidas"
+    },
+    {
+        id: 4,
+        name: "Beauty Shorts",
+        price: 110,
+        inCart: 0,
+        description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "../images/products/n6.jpg",
+        brand: "adidas"
+    },
+    {
+        id: 5,
+        name: "Dark Casual Shirt",
+        price: 150,
+        inCart: 0,
+        description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "../images/products/n8.jpg",
+        brand: "adidas"
+    },
+    // {
+    //     id: 6,
+    //     name: "Casual Green Twill Jumpsuit",
+    //     price: 180,
+    //     inCart: 0,
+    //     description:
+    //     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    //     imgSrc: "../images/products/v1.jpg",
+    //     brand: "adidas"
+    // },
+    {
+        id: 7,
+        name: "White Flower Shirt",
+        price: 150,
+        inCart: 0,
+        description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "../images/products/f2.jpg",
+        brand: "adidas"
+    },
+    {
+        id: 8,
+        name: "Vintage Flower Shirt",
+        price: 150,
+        inCart: 0,
+        description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "../images/products/f3.jpg",
+        brand: "adidas"
+    },
+    {
+        id: 9,
+        name: "Pink Flower Shirt",
+        price: 150,
+        inCart: 0,
+        description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "../images/products/f4.jpg",
+        brand: "adidas"
+    },
+    {
+        id: 10,
+        name: "Cats Long Sleeve Shirt",
+        price: 180,
+        inCart: 0,
+        description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "../images/products/f8.jpg",
+        brand: "adidas"
+    },
+    {
+        id: 11,
+        name: "Blue Social Shirt",
+        price: 211,
+        inCart: 0,
+        description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "../images/products/n1.jpg",
+        brand: "adidas"
+    },
+    {
+        id: 12,
+        name: "Grey Social Shirt",
+        price: 211,
+        inCart: 0,
+        description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "../images/products/n2.jpg",
+        brand: "adidas"
+    },
+    {
+        id: 13,
+        name: "White Social Shirt",
+        price: 211,
+        inCart: 0,
+        description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "../images/products/n3.jpg",
+        brand: "adidas"
+    },
+    {
+        id: 14,
+        name: "Map Flower Shirt",
+        price: 180,
+        inCart: 0,
+        description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "../images/products/n4.jpg",
+        brand: "adidas"
+    },
+    {
+        id: 15,
+        name: "Jeans Social Casual Shirt",
+        price: 180,
+        inCart: 0,
+        description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "../images/products/n5.jpg",
+        brand: "adidas"
+    },
+    {
+        id: 16,
+        name: "Militar Brown Casual Shirt",
+        price: 180,
+        inCart: 0,
+        description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "../images/products/n7.jpg",
+        brand: "adidas2"
+    },
+    // {
+    //     id: 17,
+    //     name: "Blue Beach Dress",
+    //     price: 110,
+    //     inCart: 0,
+    //     description:
+    //     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    //     imgSrc: "../images/products/v2.jpg",
+    //     brand: "adidas"
+    // },
+    // {
+    //     id: 18,
+    //     name: "Green Dress",
+    //     price: 110,
+    //     inCart: 0,
+    //     description:
+    //     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    //     imgSrc: "../images/products/v3.jpg",
+    //     brand: "adidas"
+    // },
+]
 
 //pegar o que seria a 'div pai', ou a nossa ul de cada produto que seria cada li 
 let productListContainer = document.getElementById('products-list');
