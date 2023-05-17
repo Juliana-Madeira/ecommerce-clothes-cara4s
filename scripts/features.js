65d1988f1a9921b1dcc806376a681ccf6501501e
Juliana Madeira <juliwood1980@gmail.com>
2023-05-17 16:43:26 -0300

    feat: try change features pics

--- scripts/features.js
+++ scripts/features.js
@@ -2,32 +2,32 @@ const features = [
     {
         id: 00,
         text: "Frete grátis",
-        img: "../images/features/f1.png"
+        img: "./images/features/f1.png"
         
     },
     {
         id: 01,
         text: "Pedido online",
-        img: "../images/features/f2.png"
+        img: "./images/features/f2.png"
     },
     {
         id: 02,
         text: "Economize",
-        img: "../images/features/f3.png"
+        img: "./images/features/f3.png"
     },
     {
         id: 03,
         text: "Promoções",
-        img: "../images/features/f4.png"
+        img: "./images/features/f4.png"
     },
     {
         id: 04,
         text: "Venda Feliz",
-        img: "../images/features/f5.png"
+        img: "./images/features/f5.png"
     },
     {
         id: 05,
         text: "Suporte 24h",
-        img: "../images/features/f6.png"
+        img: "./images/features/f6.png"
     }
 ]
